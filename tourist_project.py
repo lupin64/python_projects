c183e5b3b0d3562343b39a2b463f903fe33a43b1
Bradley Hakenson <bradley.hakenson@gmail.com>
2019-04-16 18:22:43 -0400

    Created attractions and functionality for adding new attractions

--- tourist_project.py
+++ tourist_project.py
@@ -1,7 +1,12 @@
 #lists
-destinations = ["Paris, France", "Shanghi, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+#list loop
+attractions = []
+for destination in destinations:
+    attractions.append([])
 
 #var1
 
@@ -15,8 +20,30 @@ def get_traveler_location(traveler):
   traveler_destination_index = get_destination_index(traveler_destination)
   return traveler_destination_index
 
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index].append(attraction)
+  except ValueError:
+    return
+
+#add attractions
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
 #var2
 test_destination_index = get_traveler_location(test_traveler)
 
 #print statements
 print(test_destination_index)
+
+print(attractions)
