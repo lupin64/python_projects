6835fe6d170382f95196395938af1993fd75fd44
Bradley Hakenson <bradley.hakenson@gmail.com>
2019-04-16 16:39:32 -0400

    Added test objects

---
+++ tourist_project.py
@@ -0,0 +1,4 @@
+#lists
+destinations = ["Paris, France", "Shanghi, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
